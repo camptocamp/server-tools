3fea19aeb68868b94eb835002d1c74b0fbce94e3
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-12-10 00:20:16 +0530

    [REF]

--- mass_editing/models/ir_model_fields.py
+++ mass_editing/models/ir_model_fields.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class IrModelFields(models.Model):
