f208dd6addb46ac384aeeffd5a71bac724a8afb3
sebalix <seb@usr-src.org>
2019-04-09 12:05:11 +0200

    [MIG] auditlog: Migration to 12.0

--- auditlog/__manifest__.py
+++ auditlog/__manifest__.py
@@ -3,10 +3,10 @@
 
 {
     'name': "Audit Log",
-    'version': "11.0.1.0.0",
+    'version': "12.0.1.0.0",
     'author': "ABF OSIELL,Odoo Community Association (OCA)",
     'license': "AGPL-3",
-    'website': "https://www.osiell.com",
+    'website': "https://github.com/OCA/server-tools/",
     'category': "Tools",
     'depends': [
         'base',
--- auditlog/tests/test_auditlog.py
+++ auditlog/tests/test_auditlog.py
@@ -3,7 +3,7 @@
 from odoo.tests.common import TransactionCase
 
 
-class TestAuditlog(object):
+class AuditlogCommon(object):
 
     def test_LogCreation(self):
         """First test, caching some data."""
@@ -81,7 +81,7 @@ class TestAuditlog(object):
         ]).ensure_one())
 
 
-class TestAuditlogFull(TransactionCase, TestAuditlog):
+class TestAuditlogFull(TransactionCase, AuditlogCommon):
 
     def setUp(self):
         super(TestAuditlogFull, self).setUp()
@@ -101,7 +101,7 @@ class TestAuditlogFull(TransactionCase, TestAuditlog):
         super(TestAuditlogFull, self).tearDown()
 
 
-class TestAuditlogFast(TransactionCase, TestAuditlog):
+class TestAuditlogFast(TransactionCase, AuditlogCommon):
 
     def setUp(self):
         super(TestAuditlogFast, self).setUp()
