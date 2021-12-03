e26dbcc213b9149a83ce8f340f0d83f795728996
Damien Crier <damien.crier@camptocamp.com>
2021-12-03 12:31:01 +0100

    [MIG] database_cleanup: Migration to 15.0

--- database_cleanup/tests/common.py
+++ database_cleanup/tests/common.py
@@ -2,8 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.modules.registry import Registry
-from odoo.tests import TransactionCase
-from odoo.tests.common import tagged
+from odoo.tests.common import TransactionCase, tagged
 
 
 # Use post_install to get all models loaded more info: odoo/odoo#13458
@@ -11,14 +10,3 @@ from odoo.tests.common import tagged
 class Common(TransactionCase):
     def setUp(self):
         super(Common, self).setUp()
-        # this reloads our registry, and we don't want to run tests twice
-        # we also need the original registry for further tests, so save a
-        # reference to it
-        self.original_registry = Registry.registries[self.env.cr.dbname]
-
-    def tearDown(self):
-        super(Common, self).tearDown()
-        # Force rollback to avoid unstable test database
-        self.env.cr.rollback()
-        # reset afterwards
-        Registry.registries[self.env.cr.dbname] = self.original_registry
