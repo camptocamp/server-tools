d134de4172b8a2511738125879e4c79c104b5c23
Stephane Mangin <stephane.mangin@camptocamp.com>
2022-08-31 14:47:30 +0200

    [15.0] add base_conditional_image_for_partner

---
+++ base_conditional_image_for_partner/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Conditional Images for Partners",
+    "summary": "This module add the functionality to support conditional images to partners",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Misc",
+    "depends": [
+        "base_conditional_image",
+    ],
+    "website": "https://github.com/OCA/server-tools",
+    "installable": True,
+}
